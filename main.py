ccbcfe2a89c15941c5da8670bb4a796196c40c1f
Sofia <jacobe.s@maristcollege.school.nz>
2023-08-10 14:16:05 +1200

    file dialog
    opens the file dialog in pyqt5 + a couple comments

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
+import sys
 import ebooklib
 from ebooklib import epub
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
 from PytQt5.uic import loadUiType
-import sys
+#imports
 
-class Main(QMainWindow):
+class Main(QMainWindow): #loading the gui shenanigans, so make a new class for it
     def __init__(self):
         super(Main, self). __init__()
-        loadUi("demo.ui", self)
+        loadUi("demo.ui", self) #load the specified .ui file 
 
-if __name__ == '__main__':
+        #when the 'x' button is pressed, call function connected to it
+        self.actionOpen.triggered.connect(self.openFile) 
+        self.actionOpen.triggered.connect(self.openFile)
+
+
+    def openFile(self): # new function!!!!!!!
+        fname = QFileDialog.getOpenFileName(self, 'Open File', '', 'Text files (*.txt)') # fname variable = the file opened within the dialog(directory title, which directory, specified file type)
+        self.setWindowTitle(fname[0]) # set the window title to selected fname
+        with open(fname[0], 'r') as f: # return
+            filetext = f.read()
+
+    
+
+if __name__ == '__main__': 
     app = QApplication(sys.argv)
     ui = Main()
     ui.show()
     app.exec_()
 
-# read the named file
-book = epub.read_epub('pg84.epub')
 
-# extract everything within the body content of the file, print to console
-for item in book.get_items():
+book = epub.read_epub('pg84.epub') #assign epub file to book variable
+
+for item in book.get_items(): # 
     if item.get_type() == ebooklib.ITEM_DOCUMENT:
-        print('==================================')
+        print('==================================') # these act line breaks in the console
         print('Name : ', item.get_name())
         print('----------------------------------')
         print(item.get_body_content())
