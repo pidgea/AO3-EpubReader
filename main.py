21259e3b1bbc1b273ff2273b64376b43d944baa6
pidgea <littlelostminds@gmail.com>
2023-08-18 21:58:07 +1200

    error pop up
    and also setGeometry --> resize

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QVBoxLayout, QWidget, QMessageBox
 from PyQt5.uic import loadUi
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWebEngineWidgets import QWebEngineView
@@ -17,38 +17,43 @@ class Main(QMainWindow):
         
         self.actionConvert.triggered.connect(self.convertFile) # trigger function on button press
         self.actionOpen_Window.triggered.connect(self.openHTMLWindow)
-        self.window_2 = SecondWindow() # have to mention for function below
+        self.window_2 = SecondWindow()
 
     def openHTMLWindow(self):
-        self.window_2.show() # this is basically a portal to access the next window
+        self.window_2.show() 
+        # this is basically a portal to access the next window
         # couldn't figure how to transfer fname[0] across windows
         # as in, to be able to open the HTMLViewer from the file dialog in the main window
-        # also with the way i connected the file to the GUI was convenient for time
-        # all the tutorials were for .ui's converted into .py
         
     def convertFile(self):
 
         fname = QFileDialog.getOpenFileName(self, 'Open file', '', 'EPUB files (*.epub)') # opens file dialog, epub specified
-        self.setWindowTitle(fname[0]) # sets window name to selected file's path
-        output_dir = 'output_html/' 
-        os.makedirs(output_dir, exist_ok=True) # allows creation of folder of line above 
 
-        book = epub.read_epub(fname[0]) # selected file data GET! shove into book variable
+        try:
+            self.setWindowTitle(fname[0]) # sets window name to selected file's path
+            output_dir = 'output_html/' 
+            os.makedirs(output_dir, exist_ok=True) # allows creation of folder of line above 
 
-        for item in book.get_items():
-            if item.get_type() == ebooklib.ITEM_DOCUMENT:
-                content = item.get_body_content() # book text is now in the content variable
-                
-                soup = BeautifulSoup(content, 'html.parser') # soup variable contains... the Soup (content is parsed with integrated html parser)
+            book = epub.read_epub(fname[0]) # selected file data GET! shove into book variable
 
-                content_html = str(soup) # soup is now a string in html_content
+            for item in book.get_items():
+                if item.get_type() == ebooklib.ITEM_DOCUMENT:
+                    content = item.get_body_content() # book text is now in the content variable
+                    
+                    soup = BeautifulSoup(content, 'html.parser') # soup variable contains... the Soup (content is parsed with integrated html parser)
 
-                output_filename = os.path.join(output_dir, f"{item.get_id()}.html") # outputting files with id names
+                    content_html = str(soup) # soup is now a string in html_content
 
-                with open(output_filename, 'w', encoding='utf-8') as html_file: # conversion stuff, puts the content into the files
-                    html_file.write(content_html)
-                    self.textBrowser.setText("Congrats, you converted it to HTML! Go ahead and open it by clicking 'Open Window' under 'Open HTML Window'.")
-        print("DONE") # (testing on the same epub file writes over the same html files)
+                    output_filename = os.path.join(output_dir, f"{item.get_id()}.html") # outputting files with id names
+
+                    with open(output_filename, 'w', encoding='utf-8') as html_file: # conversion stuff, puts the content into the files
+                        html_file.write(content_html)
+                        self.textBrowser.setText("Congrats, you converted it to HTML! Go ahead and open it by clicking 'Open Window' under 'Open HTML Window'.")
+            print("DONE") # (testing on the same epub file writes over the same html files)
+        except:
+            QMessageBox.critical(self, 'Error!', 'Try selecting a file!') # error pop up
+            self.setWindowTitle("WOMSE-R") # the window title changes back to 'python', so change the title again
+            return
     
 
 
@@ -59,6 +64,8 @@ class SecondWindow(QMainWindow):
         super().__init__()
         loadUi("otherui.ui", self) 
 
+        self.setWindowTitle("HTMLViewer")
+
         self.actionOpen_HTML.triggered.connect(self.openHTML)
         self.actionDark_Mode_3.triggered.connect(self.setDarkMode)
         self.actionLight_Mode_2.triggered.connect(self.setLightMode)
@@ -66,7 +73,7 @@ class SecondWindow(QMainWindow):
 
         # this is all setting up the web viewer used to display the html 
         self.web_engine_view = QWebEngineView(self)
-        self.setGeometry(100, 100, 500, 600) # it had a tendency of being really squashed, so default window size parameters :D
+        self.resize(500, 600) # it had a tendency of being really squashed, so default window size parameters :D
         self.central_layout = QVBoxLayout() # organize widget in gui
         self.central_widget = QWidget(self)
         self.central_widget.setLayout(self.central_layout)
